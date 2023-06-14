b5df3fd5d4ca58ab276474d42dc15ed2f9a5ce1a
Jose Rios <jrbernabe@gmail.com>
2023-06-13 21:16:16 -0400

    Modify addToClassName function to accept variable parameters

--- src/__tests__/utils/classNameUtils.test.ts
+++ src/__tests__/utils/classNameUtils.test.ts
@@ -8,13 +8,13 @@ describe('addToClassName function', () => {
   });
 
   it('Should not add duplicated clases to className string', () => {
-    expect(addToClassName('class1 class2', 'class2 class3')).toBe(
-      'class1 class2 class3'
+    expect(addToClassName('class1 class2', 'class2 class3', 'class3 class4')).toBe(
+      'class1 class2 class3 class4'
     );
   });
 
   it('Should no add class if empty string provided', () => {
-    expect(addToClassName('class1', '')).toBe('class1');
+    expect(addToClassName('class1', '', 'class2')).toBe('class1 class2');
     expect(addToClassName('', 'class2')).toBe('class2');
   });
 
